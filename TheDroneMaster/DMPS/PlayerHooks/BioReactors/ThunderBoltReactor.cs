bd422ab578105a7bab1d6609b755759a6ed5aef4
EvernightAurora <2465542858@qq.com>
2026-08-28 19:44:06 +0800

    尝试添加ShockwaveObject 修正skillTree线条不显示的bug

--- TheDroneMaster/DMPS/PlayerHooks/BioReactors/ThunderBoltReactor.cs
+++ TheDroneMaster/DMPS/PlayerHooks/BioReactors/ThunderBoltReactor.cs
@@ -4,13 +4,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TheDroneMaster.DMPS.DMPSutils;
 
 namespace TheDroneMaster.DMPS.PlayerHooks.BioReactors
 {
     internal class ThunderBoltReactor : DMPSBioReactor
     {
+        ShockWaveObject shockWave;
+        public static class Config
+        {
+            public const int ShockWaveEnergyRequired = 5, ShockWaveSpawnDelay = 5;
+            public const float ShockWaveRadiaus = 300f;
+            public const bool ShockWaveShowDebugHint = true;
+        }
         public ThunderBoltReactor(Player player, DMPSModule module) : base(player, module)
         {
+        }
+        public override void Update(Player player)
+        {
+            base.Update(player);
+
+            if (shockWave is not null && (shockWave.slatedForDeletetion || shockWave.room != player.room))
+            {
+                shockWave = null;
+            }
+            if (shockWave is null && ShockWaveObject.IsPlayerReady(player, this) && player.room is not null)
+            {
+                bool flag = !player.input[Config.ShockWaveSpawnDelay].spec;
+                for (int i = 0; i < Config.ShockWaveSpawnDelay; i++)
+                    flag &= player.input[i].spec;
+                if (flag)
+                {
+                    shockWave = new ShockWaveObject(player, Config.ShockWaveRadiaus, Config.ShockWaveShowDebugHint, this);
+                    player.room.AddObject(shockWave);
+                }
+            }
+            
         }
     }
 }
